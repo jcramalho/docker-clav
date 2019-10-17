c736970eac97bfcc3c2ac67e1e955b281437c5c1
jcm300 <zeclmartins@gmail.com>
2019-10-17 02:33:20 +0100

    Removed sudo

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 backup(){
-    sudo docker run --rm --volumes-from $1 -v $(pwd):/backup ubuntu bash -c "cd $2 && tar cvf /backup/$1.tar ."
+    docker run --rm --volumes-from $1 -v $(pwd):/backup ubuntu bash -c "cd $2 && tar cvf /backup/$1.tar ."
 }
 
-sudo docker stop clav_server
+docker stop clav_server
 
-sudo docker stop clav_graphdb
+docker stop clav_graphdb
 backup clav_graphdb /opt/graphdb/home/data/repositories
-sudo docker start clav_graphdb
+docker start clav_graphdb
 
-sudo docker stop clav_mongo
+docker stop clav_mongo
 backup clav_mongo /data/db
-sudo docker start clav_mongo
+docker start clav_mongo
 
-sudo docker start clav_server
+docker start clav_server
--- restore.sh
+++ restore.sh
@@ -1,15 +1,15 @@
 restore(){
-    sudo docker run --rm -v $1:$2 -v $(pwd):/backup ubuntu bash -c "cd $2 && tar xvf /backup/$3.tar"
+    docker run --rm -v $1:$2 -v $(pwd):/backup ubuntu bash -c "cd $2 && tar xvf /backup/$3.tar"
 }
 
-sudo docker stop clav_server
+docker stop clav_server
 
-sudo docker stop clav_graphdb
+docker stop clav_graphdb
 restore clav-graphdb-data /opt/graphdb/home/data/repositories clav_graphdb
-sudo docker start clav_graphdb
+docker start clav_graphdb
 
-sudo docker stop clav_mongo
+docker stop clav_mongo
 restore clav-mongo-data /data/db clav_mongo
-sudo docker start clav_mongo
+docker start clav_mongo
 
-sudo docker start clav_server
+docker start clav_server
