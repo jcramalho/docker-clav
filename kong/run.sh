a5e5058564f0f114a2fedb8345e05d49a4bdd87d
jcm300 <zeclmartins@gmail.com>
2020-05-21 22:59:26 +0100

    Adicionado CORS, headers e ACME

--- kong/run.sh
+++ kong/run.sh
@@ -15,6 +15,29 @@ if [[ $b_flag == 'true' ]]; then
     docker build -t kong-clav:1.0.0 . --no-cache
 fi
 
+ngrok http localhost:8000 --log=stdout > /dev/null &
+
+NGROK_URL=""
+while [ -z $NGROK_URL ]
+do
+    NGROK_URL=$(curl --silent http://127.0.0.1:4040/api/tunnels | grep -Po --color 'https://[0-9a-z]+\.ngrok\.io')
+done
+
+export API_VERSION="v2"
+export EMAIL="jcm300@live.com.pt"
+export DOMAINS=$NGROK_URL
+
+domains_list=""
+for domain in $DOMAINS
+do
+    domains_list+=$'\n        - '
+    domains_list+=$(echo $domain | sed -r 's/^https?:\/\///')
+done
+
+export DOMAINS_LIST=$domains_list
+
+envsubst '$$API_VERSION $$DOMAINS $$EMAIL $$DOMAINS_LIST' < kong.yml.template > kong.yml
+
 docker run -d --name kong \
     -v "$(pwd)/kong.conf:/etc/kong/kong.conf" \
     -v "$(pwd)/kong.yml:/usr/local/kong/declarative/kong.yml" \
