d463054b835ab94f07845b8b3db4e7852b58539c
jcm300 <zeclmartins@gmail.com>
2020-05-23 20:21:34 +0100

    Adicionado usage e msgs de erro na script

--- getCerts.sh
+++ getCerts.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
 
+usage(){
+    echo "USAGE: ./getCerts.sh -d <domain>"
+    echo "    -d    <domain>    Domain of certificates. REQUIRED"
+    echo "    -f    <folder>    Folder where the certificates will be stored"
+    echo "    -n                Flag that indicates to not add cronjob in crontabs"
+    echo "    -h                Help"
+}
+
 #flags
 no_cron=false
 folder=$(pwd)
-domain="*"
-while getopts ":nf:d:" o; do
+domain=""
+while getopts ":nf:d:h" o; do
     case "${o}" in
         n) no_cron=true;;
         f) folder=${OPTARG};;
         d) domain=${OPTARG};;
-        *) exit 0 ;;
+        h) usage
+           exit 0 ;;
+        *) usage
+           exit 1 ;;
     esac
 done
 
+#check if domain was inserted
+if [ -z "$domain" ]; then
+    usage
+    exit 1
+fi
+
 #install dependency if necessary
 if ! [ -x "$(command -v jq)" ]; then
-	apt-get install -y jq
+    if ! [ -x "$(command -v apt-get)" ]; then
+        echo "ERROR: Missing dependency jq..."
+        exit 1
+    else
+	    apt-get install -y jq
+    fi
 fi
 
 #get raw certificates
@@ -33,7 +55,13 @@ echo "$parsed" > $folder/fullchain.pem
 if [ $no_cron = false ]; then
     #install dependency if necessary
     if ! [ -x "$(command -v crontab)" ]; then
-        apt-get install -y cron
+        if ! [ -x "$(command -v apt-get)" ]; then
+            echo "ERROR: Missing dependency crontab."
+            echo "       Cronjob not inserted. This script will not run periodically."
+            exit 1
+        else
+            apt-get install -y cron
+        fi
     fi
 
     line="10 0 * * * $(pwd)/getCerts.sh -n -f $folder -d $domain >> /var/log/getCerts.log 2>&1"
