07c4108c87f6eac354529e3e803a30159f6f9e71
jcm300 <zeclmartins@gmail.com>
2020-07-01 19:58:36 +0100

    Some improvements in dev env

--- beforeStartDev.sh
+++ beforeStartDev.sh
@@ -6,4 +6,4 @@ apt-get update && apt-get install -y git gettext
 luarocks install --server=https://luarocks.org/manifests/jcm300 external-auth
 
 #create final db-less declarative config
-envsubst '$$API_VERSION $$API_HOST $$SERVER_AUTH_HOST' < /usr/local/kong/declarative/kong.yml.template > /usr/local/kong/declarative/kong.yml
+envsubst '$$API_VERSION $$DOMAINS $$API_HOST $$SERVER_AUTH_HOST' < /usr/local/kong/declarative/kong.yml.template > /usr/local/kong/declarative/kong.yml
